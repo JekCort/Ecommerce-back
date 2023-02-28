0f7cfc7dc196617d7c7a5a60e1079c56209e4fd2
Yevgen Kostenko <jekcort969@gmail.com>
2023-02-28 12:06:53 +0200

    Installed nodemailer. Created function forgotPasswordToken,resetPassword and added new routes

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const {
     createUser, loginUserCtrl, getAllUser, getaUser, deleteaUser, updatedUser, blockUser, unblockUser,
-    handleRefreshToken, logout, updatePassword
+    handleRefreshToken, logout, updatePassword, forgotPasswordToken, resetPassword
 } = require('../controller/userCtrl')
 const {authMiddleware, isAdmin} = require("../middlewares/authMiddleware");
 
 const router = express.Router()
 
 router.post('/register', createUser)
+router.post('/forgot-password-token', forgotPasswordToken)
+router.put('/reset-password/:token', resetPassword)
+
 router.put('/password', authMiddleware, updatePassword)
 router.post('/login', loginUserCtrl)
 router.get('/all-users', getAllUser)
